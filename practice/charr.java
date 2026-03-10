ccb744f7bdd2819b2729c435f864bbd6505dfd5d
Anish Kumar <anishkumar96084@gmail.com>
2026-03-10 23:12:46 +0530

    update

---
+++ practice/charr.java
@@ -0,0 +1,13 @@
+package practice;
+
+public class charr {
+    public static void main(String[] args){
+        char ch = 'a';
+        if(ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U' || ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
+            System.out.println("vowel");
+        } else {
+            System.out.println("consonant");
+        }
+    }
+    
+}

9d07bc57ddc07aea265ab3c74c9246a94b0df7a5
Anish Kumar <anishkumar96084@gmail.com>
2026-03-11 23:50:47 +0530

    practice1

---
+++ practice/divfive.java
@@ -0,0 +1,13 @@
+package practice;
+
+public class divfive {
+    public static void main(String[] args){
+        int num = 25;
+        if(num%5==0){
+            System.out.println("divisible by 5");
+        } else {
+            System.out.println("not divisible by 5");
+        }
+    }
+    
+}

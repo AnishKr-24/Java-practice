820d75a19fee0264c0f2bb37c571c01f3f7ea3bd
Anish Kumar <anishkumar96084@gmail.com>
2026-03-11 23:46:02 +0530

    practice

---
+++ practice/evnod.java
@@ -0,0 +1,13 @@
+package practice;
+
+public class evnod {
+
+    public static void main(String[] args){
+        int num = 10;
+        if(num%2==0){
+            System.out.println("even");
+        } else {
+            System.out.println("odd");
+        }
+    }
+}
